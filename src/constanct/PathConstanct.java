2c5642f4071a365f13fef12d09ff02fea82b8ed6
hungphd <hungphd@iastate.edu>
2019-11-29 12:12:45 -0600

    update path

--- src/constanct/PathConstanct.java
+++ src/constanct/PathConstanct.java
@@ -3,5 +3,6 @@ package constanct;
 public class PathConstanct {
 
 	public static String fop_NL_Data="/Users/hungphan/git/dataDoesNMTOutperformSMT/textNormalCorpus/english/";
+	public static String PATH_JAVA_CLASSPATH = "C:\\Program Files\\Java\\jdk1.8.0_144\\jre\\lib\\rt.jar";
 
 }
