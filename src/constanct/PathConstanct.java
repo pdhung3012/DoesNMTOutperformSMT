df3b0abcbe3d569cebf969c11e834bda1b496c62
pdhung3012 <pdhung3012@gmail.com>
2019-11-30 21:55:14 -0600

    up

--- src/constanct/PathConstanct.java
+++ src/constanct/PathConstanct.java
@@ -6,6 +6,6 @@ public class PathConstanct {
 	public static String PATH_JAVA_CLASSPATH = "C:\\Program Files\\Java\\jdk1.8.0_144\\jre\\lib\\rt.jar";
 	public static String PATH_INPUT_ABBREV_PROJECT="G:\\gitMSR13Corpus\\java_projects\\";
 	public static String PATH_OUTPUT_ABBREV_PROJECT="G:\\seqAbbrev\\";
-	public static String PATH_OUTPUT_ABBREV_CODE="G:\\seqAbbrevCombineCode\\";
+	public static String PATH_OUTPUT_ABBREV_CODE="G:\\dataAbbrevReso\\seqAbbrevCombineCode\\";
 
 }
